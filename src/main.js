88df3955155a4f74cf64bfb07642486f60de025a
Mindblowinggg <akahar36@gmail.com>
2025-08-14 01:24:31 +0530

    sd

---
+++ src/main.js
@@ -0,0 +1,8 @@
+// यह आपका JavaScript code होगा
+lottie.loadAnimation({
+  container: document.getElementById('lottie-container'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: '/Frapp logo Lottie JSON animation.json'
+});
