daa6f84ed28c7a3b10f7a6721240375004820da5
Mindblowinggg <akahar36@gmail.com>
2025-08-14 00:48:02 +0530

    cxc

--- src/main.js
+++
@@ -1,7 +0,0 @@
-lottie.loadAnimation({
-  container: document.getElementById("my-lottie-container"), // आपका div
-  renderer: "svg",
-  loop: true,
-  autoplay: true,
-  path: "src/Frapp logo Lottie JSON animation.json",
-});
