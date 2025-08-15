848c5890b8a0922aca5c2ee61947649fc2b6927f
Mindblowinggg <akahar36@gmail.com>
2025-08-15 16:56:44 +0530

    a

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
- const splashscreen = document.querySelector(".splash-screen");
- setTimeout(() => {
-  splashscreen.style.display ='none';
- }, 1000);
+const taskinput = document.getElementById("taskinput");
 
- const main= document.querySelector(".main-content");
- main.style.display='block';
+const addbtn = document.getElementById("addbtn");
+const tasklist = document.getElementById("tasklist");
+
+addbtn.addEventListener("click", () => {
+  const inputtext = taskinput.value;
+
+  if (inputtext !== "") {
+    const li = document.createElement("li");
+    li.textContent = inputtext;
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+
+    li.appendChild(deleteButton);
+    deleteButton.addEventListener("click", () => {
+      tasklist.removeChild(li);
+    });
+    tasklist.appendChild(li);
+    taskinput.value = "";
+  }
+});
